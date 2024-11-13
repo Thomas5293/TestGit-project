e9363547d16dd86a1143171f15ab2134dd70a72e
Thomas Giagkos <thomas.giagkos@hva.nl>
2024-11-13 12:12:01 +0100

    tweede taak is toegevoegd en daarmee de user story voltooit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Taak 1 is voltooid");
+        System.out.println("Taak 2 is volttooid en BTW daarmee de user story");
     }
 }
